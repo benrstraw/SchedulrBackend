78acc63177849c23a36b6d990e1e31b2ff869e60
Benjamin R. Straw <benrstraw@gmail.com>
2019-12-08 05:57:04 -0500

    Now able to grab all the courses still needed, as well as generate a semester plan (topological sort of program dependency graph, split into even size blocks).

--- Schedulr.py
+++ Schedulr.py
@@ -6,6 +6,7 @@ from sqlalchemy.dialects.mysql import insert
 import json
 import bcrypt
 from email_validator import validate_email, EmailNotValidError
+import toposort as ts
 
 app = Flask(__name__)
 app.config.from_object(schedulr_config.Config)
@@ -26,6 +27,55 @@ user_taken = db.Table('user_taken', metadata, autoload=True, autoload_with=engin
 semesters = ['FALL', 'SPRING', 'SUMMER']
 statuses = ['COMPLETE', 'INPROGRESS', 'PLANNED']
 passing = ['C', 'C+', 'B-', 'B', 'B+', 'A-', 'A', 'A+']
+
+def get_nested_prereqs(cid, dep_graph):
+	# Very important: many classes share the same prereqs, so we go down the same call tree multiple times.
+	# Solution: if the course is also in the graph, skip it. This skips not only the course call, but also the call for all it's prereqs.
+	# This is fine because if the course has already been called once, we've already processed all it's prereqs as well.
+	if cid in dep_graph:
+		return
+
+	# Retrieve from the DB the list of course IDs for the *prerequisites* for the input course ID.
+	sel = db.select([courses.c.course_id]).select_from(courses.join(course_reqs, courses.c.course_id == course_reqs.c.prereq_id)).where(course_reqs.c.course_id == cid)
+	res = connection.execute(sel)
+	db_out = res.fetchall()
+
+	# For the input course ID key in the dep_graph dictionary, add the set of prerequisite course IDs as the value.
+	dep_graph[cid] = {row['course_id'] for row in db_out}
+
+	# Since the new prerequisites we just discovered may each have their own prereqs, recursively run the call for all the courses we just added.
+	for rec in dep_graph[cid]:
+		get_nested_prereqs(rec, dep_graph)
+
+def build_deps_graph(uid):
+	# Get IDs for all of the reqsets the user has added.
+	sel = db.select([reqsets.c.rs_id]).select_from(reqsets.join(user_reqs, reqsets.c.rs_id == user_reqs.c.rs_id)).where(user_reqs.c.user_id == get_jwt_identity())
+	res = connection.execute(sel)
+	db_out = res.fetchall()
+	rsids = [row['rs_id'] for row in db_out]
+
+	# Retrieve the course IDs for all of the reqset requirements for the reqsets acquired in the previous step.
+	sel = db.select([courses.c.course_id]).select_from(courses.join(rs_reqs, courses.c.course_id == rs_reqs.c.course_id)).where(rs_reqs.c.rs_id.in_(rsids))
+	res = connection.execute(sel)
+	db_out = res.fetchall()
+	needed_courses = list(dict.fromkeys([row['course_id'] for row in db_out]))
+
+	# Retrieve all of the required courses from the DB given the list of required course IDs.
+	sel = db.select([courses]).where(courses.c.course_id.in_(needed_courses))
+	res = connection.execute(sel)
+	db_out = res.fetchall()	
+	needed_courses = [(dict(row.items())) for row in db_out]
+
+	res.close() # Close the DB connection, we're done with it!
+
+	# Create an empty dictionary to hold the dependency graph.
+	dep_graph = dict()
+
+	# For each of the courses in the reqset, call the recursive function to find it's prereqs all the way to the bottom.
+	for nc in needed_courses:
+		get_nested_prereqs(nc['course_id'], dep_graph)
+
+	return dep_graph
 
 @app.route("/list_courses", methods=['GET'])
 def list_courses():
@@ -128,6 +178,69 @@ def my_reqsets():
 	res.close()
 
 	return jsonify(userreqsets), 200
+
+@app.route("/my_needed", methods=['GET', 'POST'])
+@jwt_required
+def my_needed():
+	# Get IDs for all of the reqsets the user has added.
+	sel = db.select([reqsets.c.rs_id]).select_from(reqsets.join(user_reqs, reqsets.c.rs_id == user_reqs.c.rs_id)).where(user_reqs.c.user_id == get_jwt_identity())
+	res = connection.execute(sel)
+	db_out = res.fetchall()
+	rsids = [row['rs_id'] for row in db_out]
+
+	# Retrieve the course IDs for all of the reqset requirements for the reqsets acquired in the previous step.
+	sel = db.select([courses.c.course_id]).select_from(courses.join(rs_reqs, courses.c.course_id == rs_reqs.c.course_id)).where(rs_reqs.c.rs_id.in_(rsids))
+	res = connection.execute(sel)
+	db_out = res.fetchall()
+	needed_courses = list(dict.fromkeys([row['course_id'] for row in db_out]))
+
+	# Retrieve the course IDs, grades, and statuses for all the courses the user has taken.
+	sel = db.select([courses.c.course_id, user_taken.c.grade, user_taken.c.status]).select_from(courses.join(user_taken, courses.c.course_id == user_taken.c.course_id)).where(user_taken.c.user_id == get_jwt_identity())
+	res = connection.execute(sel)
+	db_out = res.fetchall()
+
+	# Compare the taken courses against the pass requirements, and remove passed course IDs from the list of required course IDs.
+	userpassed = [(dict(row.items())) for row in db_out]
+	for t in userpassed:
+		if t['status'] == 'COMPLETE' and t['grade'] in passing:
+			try:
+				needed_courses.remove(t['course_id'])
+			except ValueError as e:
+				pass
+
+	# Retrieve all of the required courses from the DB given the list of required course IDs.
+	sel = db.select([courses]).where(courses.c.course_id.in_(needed_courses))
+	res = connection.execute(sel)
+	db_out = res.fetchall()	
+	needed_courses = [(dict(row.items())) for row in db_out]
+
+	res.close()
+
+	return jsonify(needed_courses), 200
+
+@app.route("/gen_schedule", methods=['GET', 'POST'])
+@jwt_required
+def gen_schedule():
+	max_classes = 4
+
+	if request.is_json:
+		max_classes = request.json.get('max_classes')
+
+	dep_graph = build_deps_graph(get_jwt_identity())
+
+	# Run topological sort on the final dependency graph.
+	toposorted = ts.toposort(dep_graph, max_classes)
+	course_groups = [list(row) for row in toposorted]
+
+	output = []
+	for cg in course_groups:
+		# Retrieve all of the required courses from the DB given the list of required course IDs.
+		sel = db.select([courses]).where(courses.c.course_id.in_(cg))
+		res = connection.execute(sel)
+		db_out = res.fetchall()	
+		output += [[(dict(row.items())) for row in db_out]]
+
+	return jsonify(output), 200
 
 @app.route("/add_taken", methods=['POST'])
 @jwt_required
